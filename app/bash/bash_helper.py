fa46e979b5fa8839808612201ac77523389c9ef6
Libo Liu <liboliu@usc.edu>
2019-01-31 21:58:52 -0800

    cost of sb

--- app/bash/bash_helper.py
+++ app/bash/bash_helper.py
@@ -286,9 +286,9 @@ def update_bash_status(bash_id, job_id, logs, rq_connection):
             layer_modified_at = find_modified_at_by_md5vector(md5vector, db_session)
 
         from rq import get_current_job
-        from dateutil import tz
+        
         job = get_current_job()
-        job_enqueued_at = job.enqueued_at.astimezone(tz.tzlocal())
+        job_enqueued_at = job.enqueued_at.astimezone(tz=None)
         time_comparision = "\njob_enqueued_at: %s\nlayer_modified_at: %s" % (datetime.strftime(job_enqueued_at,'%Y-%m-%d %H:%M:%S'), datetime.strftime(layer_modified_at,'%Y-%m-%d %H:%M:%S'))
         if job_enqueued_at > layer_modified_at:
             return "Failed to run the command, the job enqueued_at time is later than the layer updated." + time_comparision
